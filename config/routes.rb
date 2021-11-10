1af3336e73c97411356e9a36880d1d4ec48436dd
postmartem <paula.marten@student.hpi.de>
2021-11-10 15:11:59 +0100

    Joa

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # You can have the root of your site routed with "root"
+  resources :authors
+
   root 'home#index'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):

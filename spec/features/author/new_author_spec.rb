48d78b23fd0cc11b5e99316ddec97aee40511f9c
postmartem <paula.marten@student.hpi.de>
2021-11-10 16:05:30 +0100

    added test for issue3

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,10 @@ require 'rails_helper'
      # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
      visit new_author_path
    end
+   it "should have text inputs for an author's frst name, last name, and homepage" do
+    visit new_author_path
+    expect(page).to have_field('author[first_name')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage')
+   end
  end

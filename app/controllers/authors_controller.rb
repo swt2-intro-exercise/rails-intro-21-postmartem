64f1f75bfa415f8ab5c2ab42ef68f56b63271ef8
postmartem <paula.marten@student.hpi.de>
2021-11-16 17:22:42 +0100

    fix #7

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,12 +1,21 @@
 class AuthorsController < ApplicationController
+    def show
+        @author = Author.find(params[:id])
+    end
+
     def new
     end
+
+    def index
+        @authors = Author.all
+    end
+
     def create
         #render plain: params[:article].inspect
         @author = Author.new(author_params)
 
         @author.save
-        redirect_to root_path, notice: 'Success!'
+        redirect_to author_path(@author), notice: 'Success!'
     end
 
     private

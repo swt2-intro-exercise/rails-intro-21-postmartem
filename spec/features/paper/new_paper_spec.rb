3c4282d0aca184c55631713f35f895978ff8d05a
postmartem <paula.marten@student.hpi.de>
2021-11-18 16:17:22 +0100

    fix #15

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
- describe "New author page", type: :feature do
+ describe "New paper page", type: :feature do
    it "should exist at 'new_paper_path' and render withour error" do
      visit new_paper_path
     
    end
+
+   it 'should not accept papers without a last name' do
+    @paper = Paper.new(venue:"mind 49: 433-460", year:1950)
+    expect(@paper).to_not be_valid
+   end
+
   end
-  
